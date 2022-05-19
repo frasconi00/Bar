eab7e72bdbaac01569cde4af273d20dca8058d09
samue <samue@LAPTOP-NDSNVVOI.homenet.telecomitalia.it>
2022-05-19 14:52:24 +0200

    Manca solo il testModel

--- src/main/java/it/polito/tdp/bar/model/Model.java
+++ src/main/java/it/polito/tdp/bar/model/Model.java
@@ -1,5 +1,15 @@
 package it.polito.tdp.bar.model;
 
 public class Model {
+	private Simulator sim;
+	
+	public Model() {
+		this.sim = new Simulator();
+	}
+	
+	public void simula() {
+		this.sim.init();
+		this.sim.run();
+	}
 
 }
